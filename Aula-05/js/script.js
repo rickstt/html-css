fee33b2824e7c110e1c18907f83570c192f7cc01
Henrique Santos <henrique.sespindola@senacsp.edu.br>
2023-06-12 11:55:08 -0300

    Feat: Start edição de usuários

--- Aula-05/js/script.js
+++ Aula-05/js/script.js
@@ -4,6 +4,7 @@ const txtuser = document.getElementById("txtuser");
 const senha = document.getElementById("senha");
 let autenticado;
 let token;
+let key; 
 
 btn.onclick = () => {
     //validação dos campos usuário e senha
@@ -29,6 +30,8 @@ btn.onclick = () => {
                 token = dado.token;
                 txtuser.value = "";
                 senha.value = "";
+                //Alterar a tela
+                window.location.replace(`list.html?key+${token}`);
             }
             else {
                 txtuser.value = "";
@@ -92,8 +95,11 @@ btnCad.onclick = () => {
     }
 }
 
-//
+
 function carregarDados() {
+
+    key = window.location.search.substring(5,window.location.search.length);
+
     const estrutura = document.getElementById("estrutura");
     fetch("http://127.0.0.1:8087/users/list").then((response) => response.json()).then((result) => {
         result.data.map((item, index) => {
@@ -104,13 +110,85 @@ function carregarDados() {
                 <h3>${item.idusuario}</h3>
                 <h2>${item.nomeusuario}</h2>
                 <h3>${item.email}<h3>
-                <a href="#" onclick="editar('${item.idusuario}', '${item.nomeusuario}')"><img src="assets/editar.jpg"></a>
+                <a href="#" onclick="editar('${item.idusuario}', '${item.nomeusuario}', '${item.email}', '${item.foto}')"><img src="assets/editar.jpg"></a>
             `;
             estrutura.appendChild(divUser);
         })
     }).catch((error) => console.log(`Erro ao executar API -> ${error}`))
 }
 
-function editar(id, usuario) {
-    alert(`O id é ${id} e o nome é ${usuario}`);
+function editar(id, usuario, email, foto) {
+    //Fazer uma referência ao body da página html
+    const body = document.body;
+    const div_shadow = document.createElement("div");
+    const div_white = document.createElement("div");
+    const form = document.createElement("form");
+    const input_id = document.createElement("input");
+    const input_user = document.createElement("input");
+    const input_pass = document.createElement("input");
+    const input_confirm = document.createElement("input");
+    const input_email = document.createElement("input");
+    const input_file = document.createElement("input");
+    const input_sub = document.createElement("input");
+
+    //Aplicar atributos aos elementos
+    div_shadow.setAttribute("id","div_shadow");
+    div_white.setAttribute("id","div_white");
+    //atributo para não enviar o formulário, o envio será feito via JS
+    form.setAttribute("onsubmit","return false");
+    //Aplicar atributos as caixas de input
+    input_id.setAttribute("type", "text");
+    input_id.setAttribute("placeholder", `Id do Usuário: ${id}`);
+    input_id.setAttribute("disabled", "true");
+
+    input_user.setAttribute("type", "text");
+    input_user.setAttribute("placeholder", `Nome de Usuário: ${usuario}`);
+    input_user.setAttribute("disabled", "true");
+
+    input_pass.setAttribute("type", "password");
+    input_pass.setAttribute("placeholder", "Digite a nova senha");
+    input_confirm.setAttribute("type", "password");
+    input_confirm.setAttribute("placeholder", "Confirme a nova senha");
+
+    input_email.setAttribute("type", "email");
+    input_email.setAttribute("placeholder", `${email}`)
+
+    input_file.setAttribute("type",  "file");
+    input_file.setAttribute("value", `${foto}`);
+
+    input_sub.setAttribute("type", "submit");
+    input_sub.setAttribute("value", "Update")
+
+    input_sub.onclick = () => {
+        if (input_confirm.value != input_pass.value){
+            return alert(`As senhas não coincidem`);
+        }
+        else {
+            fetch(`http://127.0.0.1:8087/users/update/${id}`, {
+                method: "PUT",
+                headers: {
+                    "accept":"application/json",
+                    "content-type":"application/json",
+                    "token":key
+                },
+                body:JSON.stringify({
+                    senha: input_pass.value,
+                    email: input_email.value,
+                    foto: input_file.value
+                })
+            });
+        }
+    }
+
+    form.appendChild(input_id);
+    form.appendChild(input_user);
+    form.appendChild(input_pass);
+    form.appendChild(input_confirm);
+    form.appendChild(input_email);
+    form.appendChild(input_file);
+    form.appendChild(input_sub);
+    div_white.appendChild(form);
+    div_shadow.appendChild(div_white);
+    body.appendChild(div_shadow);
+
 }
